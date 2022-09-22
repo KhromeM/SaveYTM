664bf6452535758db59dca1bf282db9d442e4292
KhromeM <68210337+KhromeM@users.noreply.github.com>
2022-09-22 13:39:30 -0400

    typo

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ const app = require("./routes.js");
 const CONFIG = require("./config.js");
 
 app.listen(CONFIG.port, () => {
-	`console`.log("Server is listening to port: " + CONFIG.port);
+	console.log("Server is listening to port: " + CONFIG.port);
 });
