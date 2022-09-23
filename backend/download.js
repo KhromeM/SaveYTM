822c3a39dddb4fcf78268dc9e830007b7faebb2d
KhromeM <68210337+KhromeM@users.noreply.github.com>
2022-09-23 15:07:05 -0400

    aws stuff

--- backend/download.js
+++ backend/download.js
@@ -1,9 +1,26 @@
 const ytdl = require("ytdl-core");
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3();
+const stream = require("stream");
+const { fstat } = require("fs");
 
 const func = async (videoID) => {
-	let info = await ytdl.getInfo(videoID);
-	let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-	console.log(audioFormats);
+	upload(ytdl(videoID), "youtubevideo.mp4");
 };
 
-func("cULQhvuq1Zc");
+const upload = (stream, name) => {
+	// let pass = new stream.PassThrough();
+
+	let params = { Bucket: "saveytm", Key: name, Body: stream };
+	S3.upload(params, (err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	});
+};
+
+// func("cULQhvuq1Zc");
+// https://saveytm.s3.amazonaws.com/youtubevideo.mp4
+const download = () => {};
